93d57e7fbf39bb3c05f6ff89a3e758027626c80d
moob <moob10293@gmail.com>
2021-05-03 10:52:53 -0700

    Add files via upload

--- executethis.java
+++ executethis.java
@@ -1,13 +1,14 @@
 package plugin.somefile.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class main extends JavaPlugin{
+public class main extends JavaPlugin{//
     @Override
     public void onEnable(){
-        System.out.printf("Hello, world!");
+        getServer().getPluginManager().registerEvents(new Eating(),this);//using the eat plugin
+        getServer().getPluginManager().registerEvents(new whenDamage(),this);//using the damgage plugin
     }
     @Override
     public void onDisable(){
-        System.out.printf("Hello, world!");
+
     }
 }
--- scanner.java
+++ scanner.java
@@ -16,7 +16,7 @@ public class Eating impements Listener{
         ItemStack thingeaten = event.getItem();
         Player person=event.getPlayer();
         if (thingeaten.getType()==Material.ROTTEN_FLESH){//rotten flesh
-            event.setCancelled(true);//I don't know what this thing does, I copied it from a video on youtube
+            event.setCancelled(true);//no, I don't want to add hunger on top of hunger
             thingeaten.setType(Material.AIR);//gets rid of the food that you ate(possible bug, gets rid of all the food in the stack)
             PotionEffect owch=new PotionEffect(PotionEffectType.POISON,10,2);//initalises the effects to give
             PotionEffect hungry=new PotionEffect(PotionEffectType.HUNGER,30,1);
@@ -25,25 +25,16 @@ public class Eating impements Listener{
             person.setFoodLevel(person.getFoodLevel()+4);//applies the hunger rotten flesh restores(no change here)
         }
         else if (thingeaten.getType()==Material.CHICKEN){
-            person.setFoodLevel(person.getFoodLevel()+2);
+            event.setCancelled(true);
+            person.setFoodLevel(person.getFoodLevel()+2);//no change here either
             thingeaten.setType(Material.AIR);
             PotionEffect hungry=new PotionEffect(PotionEffectType.HUNGER,30,1);//the chance used to be 30%, now it's 100%
             person.addPotionEffect(hungry);
         }
-        else if (thingeaten.getType()==Material.COOKED_CHICKEN){
-            thingeaten.setType(Material.AIR);
-            person.setFoodLevel(person.getFoodLevel()+5);//1 less than what it used to be(6 hunger)
+        else {
+            setFoodLevel(person.getFoodLevel()-1)
         }
-        else if (thingeaten.getType()==Material.BEEF){
-            thingeaten.setType(Material.AIR);
-            person.setFoodLevel(person.getFoodLevel()+2);//1 less than what it used to be(3 hunger)
         }
-        else if (thingeaten.getType()==Material.COOKED_BEEF){
-            thingeaten.setType(Material.AIR);
-            person.setFoodLevel(person.getFoodLevel()+7);//1 less than what it used to be(6 hunger)
-        }
-        //I think you get what the rest is supposed to be like. unfortunantly, bukkit has no method or class relating to food, so 
-        //I can't use something that applies for all. I'll finish these after the rest
     }
     
 }
